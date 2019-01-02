454fe07d4935823643ef48c2e35bac96f734240b
Zak Timson <zaktimson@gmail.com>
2019-01-01 22:53:19 -0500

    Cleaned up dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,4 +10,17 @@ export class DashboardComponent {
     now = timer(0, 1000).pipe(map(() => new Date()));
 
     constructor() { }
+
+    formatTime(date: Date) {
+        if(!date) return '';
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        if(hours > 12) hours -= 12;
+        return `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}`
+    }
+
+    formatDate(date: Date) {
+        if(!date) return '';
+        return `${date.toDateString().substr(4, 3)} ${date.getDate()}`;
+    }
 }
