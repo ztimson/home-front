d388e8b73dbdcf210c1cad0c439d1de9654dae99
Zak Timson <zaktimson@gmail.com>
2019-01-01 22:11:03 -0500

    Added placeholder security screen

--- src/app/security/security.component.ts
+++ src/app/security/security.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-security',
-  templateUrl: './security.component.html'
+    selector: 'app-security',
+    templateUrl: './security.component.html'
 })
-export class SecurityComponent implements OnInit {
+export class SecurityComponent {
+    armed: boolean = false;
+    log = [
+        {timestamp: new Date(), message: 'Currently under construction'},
+        {timestamp: new Date(), message: 'Give the power button a flick!'},
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    toggle() {
+        this.armed = !this.armed;
+        this.log = [{timestamp: new Date(), message: this.armed ? 'Arming Security' : 'Disengaged'}].concat(this.log);
+    }
 }
